574352d8a2ad59451766b23b441858ca040e40e4
Chloe Caron <cmlc3@cam.ac.uk>
2018-10-19 13:48:34 +0100

    Started package identifier code

---
+++ IDP_programs_prototypes/package_identifier.cc
@@ -0,0 +1,29 @@
+include <iostream>
+#include <robot_instr.h>
+#include <robot_link.h>
+#include <stdlib.h>     //for using the function sleep
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+#include <stopwatch.h>
+#include <robot_delay.h>
+#include <cmath>
+
+#define ROBOT_NUM 14   // The id number (see below)
+
+using namespace std;
+
+int main () {
+	robot_link rlink; 
+
+	if (!rlink.initialise (ROBOT_NUM)) { 						// Setup the link
+			cout << "Cannot initialise link" << endl;
+			rlink.print_errs("  ");
+			return -1;
+		}
+		else {
+			cout << "Initialised"<< endl;
+		}
+}
+	
+	
